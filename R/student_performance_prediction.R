b5917e12245c9b299ffe7ed34902c6be459c077e
kdpnimsara <kdpnimsara@students.nsbm.ac.lk>
2026-09-11 00:02:13 +0530

    Add data preprocessing and target variable

--- R/student_performance_prediction.R
+++ R/student_performance_prediction.R
@@ -1,18 +1,12 @@
---------------------------------------------------
+#--------------------------------------------------
 # A/L STUDENT PERFORMANCE PREDICTION
---------------------------------------------------
+#--------------------------------------------------
+  data <- read.csv("C:/Users/USER/OneDrive/Documents/AL-Student-Performance-Prediction-R-PROJECT-/data/2020_al_data_kaggle_upload_new_old_syllabi.csv")
 
 
-# IMPORT DATA
---------------------------------------------------
-
-data <- read.csv(
-  "data/2020_al_data_kaggle_upload_new_old_syllabi.csv"
-)
-
-
+  
 # DATASET EXPLORATION
---------------------------------------------------
+#--------------------------------------------------
 
 # dataset structure
 str(data)
@@ -31,7 +25,7 @@ summary(data)
 
 
 # CHECKING DATASET
---------------------------------------------------
+#--------------------------------------------------
 
 # dataset size
 dim(data)
@@ -44,3 +38,47 @@ colSums(is.na(data))
 
 # dataset summary
 summary(data)
+
+
+#data preprocessing
+#----------------------------------------------------
+
+#convert Zscore to numeric
+data$Zscore <- as.numeric(data$Zscore)
+
+#check Zscore
+head(data$Zscore)
+
+#convert gender to factor
+data$gender <- as.factor(data$gender)
+
+#convert stream to factor
+data$stream <- as.factor(data$stream)
+
+#convert syllabus to factor
+data$syllabus <- as.factor(data$syllabus)
+
+#check dataset
+str(data)
+
+
+#target variable
+#---------------------------------
+
+data$Performance <- ifelse(
+  data$Zscore >= 0,
+  "High",
+  "Low"
+)
+
+#convert target variable to factor
+#-------------------------------------
+
+data$Performance <- as.factor(data$Performance)
+
+#check target variable
+#----------------------------------
+
+head(data$Performance)
+
+table(data$Performance)
